cd7dc050ae7d7d77d132c1ce7231426901af2cb7
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2023-09-10 16:32:06 +0600

    UI Template added

--- js/main.js
+++ js/main.js
@@ -43,15 +43,15 @@ const displayPhones = (phones, isShowAll) =>{
         phoneCard.classList = 'card bg-base-100 border rounded-xl text-primary p-6'
         phoneCard.innerHTML = `
         <figure class=" bg-[#0D6EFD0D] h-72 flex w-full justify-center items-center p-8 rounded-lg">
-            <img src="${phone.image}" alt="${phone.phone_name}" class="rounded-xl w-3/5" />
+            <img src="${phone?.image}" alt="${phone?.phone_name}" class="rounded-xl w-3/5" />
         </figure>
         <div class="card-body items-center text-center pb-0">
-            <h4 class="text-sm">${phone.brand}</h4>
-            <h2 class="card-title font-bold mb-4">${phone.phone_name}</h2>
+            <h4 class="text-sm">${phone?.brand}</h4>
+            <h2 class="card-title font-bold mb-4">${phone?.phone_name}</h2>
             <p>There are many variations of passages of available, but the majority have suffered</p>
             <p class="font-bold text-xl">$<span>999</span></p>
             <div class="card-actions">
-                <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-ghost text-white bg-primary hover:bg-blue-500 normal-case">Show Details</button>
+                <button onclick="handleShowDetail('${phone?.slug}')" class="btn btn-ghost text-white bg-primary hover:bg-blue-500 normal-case">Show Details</button>
             </div>
         </div>
         `
@@ -77,15 +77,15 @@ const showPhoneDetails = (phone) => {
     madalDialogue.innerHTML = `
     <form method="dialog" class="modal-box">
         <figure class="bg-[#0D6EFD0D] h-72 rounded-lg flex w-full justify-center items-center p-8">
-            <img src="${phone.image}" alt="" class="w-2/5">
+            <img src="${phone?.image}" alt="" class="w-2/5">
         </figure>
-        <h3 class="font-bold text-lg mt-6">${phone.name}</h3>
+        <h3 class="font-bold text-lg mt-6">${phone?.name}</h3>
         <p class="py-4 text-sm">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-        <p class="py-2"><span class="font-semibold">Storage :</span> ${phone.mainFeatures?.storage || 'No data found'}</p>
-        <p class="py-2"><span class="font-semibold">Display Size :</span> ${phone.mainFeatures?.displaySize  || 'No data found'}</p>
-        <p class="py-2"><span class="font-semibold">Chipset :</span> ${phone.mainFeatures?.chipSet  || 'No data found'}</p>
-        <p class="py-2"><span class="font-semibold">Memory :</span> ${phone.mainFeatures?.memory  || 'No data found'}</p>
-        <p class="py-2"><span class="font-semibold">Slug :</span> ${phone.slug  || 'No data found'}</p>
+        <p class="py-2"><span class="font-semibold">Storage :</span> ${phone?.mainFeatures?.storage || 'No data found'}</p>
+        <p class="py-2"><span class="font-semibold">Display Size :</span> ${phone?.mainFeatures?.displaySize  || 'No data found'}</p>
+        <p class="py-2"><span class="font-semibold">Chipset :</span> ${phone?.mainFeatures?.chipSet  || 'No data found'}</p>
+        <p class="py-2"><span class="font-semibold">Memory :</span> ${phone?.mainFeatures?.memory  || 'No data found'}</p>
+        <p class="py-2"><span class="font-semibold">Slug :</span> ${phone?.slug  || 'No data found'}</p>
         <div class="modal-action justify-center">
             <button class="btn btn-outline">Close</button>
         </div>
